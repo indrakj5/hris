8147bd157bdac10643ca7eecbe2a383b9d3ea428
indrakj5 <indrakomara13@gmail.com>
2018-09-30 20:57:28 +0700

    job

--- module/recruitment/job.php
+++ module/recruitment/job.php
@@ -31,15 +31,105 @@
               <div class="title_left">
                 <h3>Daftar Lowongan Pekerjaan</h3>
               </div>
-
-              
-            </div>
+			</div>
 
             <div class="clearfix"></div>
 
             <div class="row">
               <div class="col-md-12 col-sm-12 col-xs-12">
-                
+                <div class="form-group" align="left">
+										<label> Divisi </label>
+										<div class="row">
+											<div class="col-lg-6 col-md-6 col-sm-6">
+											<select name="Divisi" class="form-control" required>
+												<option selected="selected" disabled="disabled">- Pilih Divisi -</option>
+											<?php
+												$Query	=	mysql_query("SELECT * FROM tbl_m_status_pernikahan");
+												while($f=	mysql_fetch_array($Query)){
+											?>
+												<option value="<?php echo $f['id_status']; ?>"><?php echo $f['status']; ?></option>
+											<?php
+												}
+											?>
+											</select>
+											</div>
+										</div>
+				</div>
+				
+				<div class="form-group" align="left">
+										<label> Jabatan </label>
+										<div class="row">
+											<div class="col-lg-6 col-md-6 col-sm-6">
+											<select name="Divisi" class="form-control" required>
+												<option selected="selected" disabled="disabled">- Pilih Jabatan -</option>
+											<?php
+												$Query	=	mysql_query("SELECT * FROM tbl_m_status_pernikahan");
+												while($f=	mysql_fetch_array($Query)){
+											?>
+												<option value="<?php echo $f['id_status']; ?>"><?php echo $f['status']; ?></option>
+											<?php
+												}
+											?>
+											</select>
+											</div>
+										</div>
+				</div>
+				
+				<div class="form-group" align="left">
+										<label> Status </label>
+										<div class="row">
+											<div class="col-lg-6 col-md-6 col-sm-6">
+											<select name="Divisi" class="form-control" required>
+												<option selected="selected" disabled="disabled">- Pilih Status -</option>
+											<?php
+												$Query	=	mysql_query("SELECT * FROM tbl_m_status_pernikahan");
+												while($f=	mysql_fetch_array($Query)){
+											?>
+												<option value="<?php echo $f['id_status']; ?>"><?php echo $f['status']; ?></option>
+											<?php
+												}
+											?>
+											</select>
+											</div>
+										</div>
+				</div>
+				
+				<div class="form-group" align="left">
+										<label> Pendidikan </label>
+										<div class="row">
+											<div class="col-lg-6 col-md-6 col-sm-6">
+											<select name="Divisi" class="form-control" required>
+												<option selected="selected" disabled="disabled">- Pilih Pendidikan -</option>
+											<?php
+												$Query	=	mysql_query("SELECT * FROM tbl_m_status_pernikahan");
+												while($f=	mysql_fetch_array($Query)){
+											?>
+												<option value="<?php echo $f['id_status']; ?>"><?php echo $f['status']; ?></option>
+											<?php
+												}
+											?>
+											</select>
+											</div>
+										</div>
+				</div>
+				
+				<div class="form-group" align="left">
+										<label> Skill Requirement </label>
+										<div class="row">
+											<div class="col-lg-6 col-md-6 col-sm-6">
+											
+											</div>
+										</div>
+				</div>
+				
+				<div class="form-group" align="left">
+										<label>  Deskripsi Pekerjaan </label>
+										<div class="row">
+											<div class="col-lg-6 col-md-6 col-sm-6">
+											
+											</div>
+										</div>
+				</div>
               </div>
             </div>
           </div>
